1b96d660092b77d2ad511d1251a7640d8db3c0ae
dash01 <damir@shakirov.tv>
2023-11-26 18:51:57 +0300

    Выполнено задание №2

--- src/Homework1.java
+++ src/Homework1.java
@@ -2,7 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Homework1 {
     public static void main(String[] args) {
-        //+++Задание №1+++
+        System.out.println("+++Задание №1+++");
         ///Дана строка
         String name = "     ПЕтРов Олег Иванович     ";
         //Необходимо
@@ -20,30 +20,42 @@ public class Homework1 {
         } else {
             System.out.println("Неизвестное лицо " + name);
         }
-        //+++Задание №2+++
+
+        System.out.println("\n+++Задание №2+++");
         //У нас есть машина. В данной машине есть перечень проверок, перед запуском
         //Количество топлива
         double fuel = 10;
         //Проверка двигателя
-        boolean isEngineWork = true;
+        boolean isEngineWork = false;
         //Проверка отсутствия ошибок (false - ошибок нет)
         boolean hasErrors = false;
         //Датчики давления шин
         boolean isWheelWork1 = true;
         boolean isWheelWork2 = true;
         boolean isWheelWork3 = true;
-        boolean isWheelWork4 = true;
+        boolean isWheelWork4 = false;
 
         //Поменять(убрать, поставить) логические операторы так, чтобы машина запускалась:
         // когда топлива не меньше 10 литров, двигатель работает, колеса все работают, нет ошибок
         //В ином случае, машина не должна запускаться
-        if (
-                fuel < 10
-                        ||  (!isWheelWork1 || isWheelWork2 || isWheelWork3 || isWheelWork4)
-                        && hasErrors
-                        || isEngineWork
-        ) {
+        if (fuel >= 10 && isEngineWork && isWheelWork1 && isWheelWork2 && isWheelWork3 && isWheelWork4 && !hasErrors) {
             System.out.println("Машина работает");
-        } else System.out.println("Машина не работает");
+        } else {
+            System.out.println("Машина не работает:");
+            if (fuel < 10 ) {
+                System.out.println("Топлива меньше 10 литров");
+            }
+            if (!isEngineWork) {
+                System.out.println("Неисправность в двигателе");
+            }
+            if (!hasErrors) {
+                System.out.println("Обнаружены ошибки");
+            }
+            if (!isWheelWork1 || !isWheelWork2 || !isWheelWork3 || !isWheelWork4) {
+                System.out.println("Проверьте давление в шинах");
+            }
+        }
+        System.out.println("\n+++Задание №3+++");
+
     }
 }
